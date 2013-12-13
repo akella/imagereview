6eb2f1cb70cee67671312260670df98d4d51bc3c
akella <akella@Yuris-MacBook-Pro.local>
2013-12-13 17:55:59 +0200

    gogo

--- js/common.js
+++ js/common.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+	$('.js-togglesidebar').click(function(event) {
+		$('body').toggleClass('is-hiddensidebar');
+		$(this).toggleClass('is-active');
+		return false;
+	});
 });
